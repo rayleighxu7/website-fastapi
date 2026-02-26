da72ac6fc462df280b31a3b2e3b27339056dac10
rayleigh <rayleigh.xu@getcaruso.com>
2026-02-26 22:07:19 +1300

    third animation fix

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
@@ -6,7 +8,12 @@ from app.config import BASE_DIR
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
+# Cache-bust static assets on each deploy
+_ASSET_VERSION = str(int(time.time()))
+
 
 @router.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "v": _ASSET_VERSION}
+    )
